6e4a85ed98a4e94a88f92974e34908f8172a8fa9
bradlindblad <bradley.lindblad@gmail.com>
2018-06-02 20:06:08 -0500

    organized script into functions

--- youtube_truth.R
+++ youtube_truth.R
@@ -1,67 +1,125 @@
-
-#
-# Run from the command line
-# Arg[1]: search term, ex: "dogs"
-# Arg[2]: number of videos to iterate through ex: 23
-#
-# Example cmd line '''   Rscript youtube_truth.R "dogs" 23   '''
-#
-# Note: you will need to add your R binary to your PATH to run from command line
-#
-
-args <- commandArgs(trailingOnly = TRUE)
-
-search <- ""#args[1]
-
-number.of.vids <- 75 #args[2]
-
+###################
+#### LIBRARIES ####
+###################
 
 library(RSelenium)
 library(tidyverse)
 
-# Set up
-driver <- rsDriver(browser = c("chrome"))
-remDr <- driver[["client"]]
-#remDr$navigate(paste0("https://www.youtube.com/results?search_query=",search))
-# alternativley, to start from the first suggested youtube vid, use this:
-remDr$navigate("https://www.youtube.com")
-Sys.sleep(2)
+###################
+##### DEFINE ######
+###### FNS ########
+###################
 
-# Create data frame to append all the titles to
-all.titles <- data.frame(Name = "Initialize")
+search.vids <- function(search, number.of.vids) {
+    # function that allows you to enter a search term in quotation and specify the number of vids you want it to cycle through
+    # Set up
+    driver <- rsDriver(browser = c("chrome"))
+    remDr <- driver[["client"]]
+    remDr$navigate(paste0("https://www.youtube.com/results?search_query=",search))
+    Sys.sleep(2)
+    
+    # Create data frame to append all the titles to
+    all.titles <- data.frame(Name = "Initialize")
+    
+    # Click on first vid
+    first.vid <- remDr$findElement(using = 'xpath', value = '//*[@id="video-title"]')
+    first.vid$clickElement()
+    Sys.sleep(9)
+    
+    
+    # Begin loop
+    i = 1
+    while (i < number.of.vids) {
+      Sys.sleep(1)
+      vid.title <- remDr$findElement(using = 'xpath', value = "//*[@id='container']/h1/yt-formatted-string")
+      vid.title <- vid.title$getElementText() %>% as.character()
+      vid.title <- data.frame(Name = vid.title)
+      all.titles <- rbind(all.titles, vid.title)
+      Sys.sleep(3)
+      # Next vid
+      remDr$sendKeysToActiveElement(list(key = "shift", "N"))
+      i = i + 1
+      Sys.sleep(sample(seq(7, 15, 1), 1))
+    }
+    
+    write.csv(all.titles, paste0("C:\\Users\\XPS\\Desktop\\",
+                                 gsub(pattern = ":", replacement = "_",
+                                      gsub(pattern = " ", replacement = "_", Sys.time())), 
+                                 search,
+                                 "_",
+                                 number.of.vids,
+                                 "vids",
+                                 ".csv")
+    )
+    
+    remDr$close()
 
-
-
-# Click on first vid
-first.vid <- remDr$findElement(using = 'xpath', value = '//*[@id="video-title"]')
-first.vid$clickElement()
-Sys.sleep(9)
-
-
-# Begin loop
-i = 1
-while (i < number.of.vids) {
-  Sys.sleep(1)
-  vid.title <- remDr$findElement(using = 'xpath', value = "//*[@id='container']/h1/yt-formatted-string")
-  vid.title <- vid.title$getElementText() %>% as.character()
-  vid.title <- data.frame(Name = vid.title)
-  all.titles <- rbind(all.titles, vid.title)
-  Sys.sleep(3)
-  # Next vid
-  remDr$sendKeysToActiveElement(list(key = "shift", "N"))
-  i = i + 1
-  Sys.sleep(sample(seq(7, 15, 1), 1))
 }
 
-write.csv(all.titles, paste0("C:\\Users\\XPS\\Desktop\\",
-                             gsub(pattern = ":", replacement = "_",
-                                  gsub(pattern = " ", replacement = "_", Sys.time())), 
-                             search,
-                             "_",
-                             number.of.vids,
-                             "vids",
-                             ".csv")
-)
+start.fresh <- function(number.of.vids) {
+    # function that starts with a fresh Chrome session and iterates through whatever vids Youtube recommends
+    # Set up
+    driver <- rsDriver(browser = c("chrome"))
+    remDr <- driver[["client"]]
+    remDr$navigate("https://www.youtube.com")
+    Sys.sleep(2)
+    
+    # Create data frame to append all the titles to
+    all.titles <- data.frame(Name = "Initialize")
+    
+    # Click on first vid
+    first.vid <- remDr$findElement(using = 'xpath', value = '//*[@id="video-title"]')
+    first.vid$clickElement()
+    Sys.sleep(9)
+    
+    
+    # Begin loop
+    i = 1
+    while (i < number.of.vids) {
+      Sys.sleep(1)
+      vid.title <- remDr$findElement(using = 'xpath', value = "//*[@id='container']/h1/yt-formatted-string")
+      vid.title <- vid.title$getElementText() %>% as.character()
+      vid.title <- data.frame(Name = vid.title)
+      all.titles <- rbind(all.titles, vid.title)
+      Sys.sleep(3)
+      # Next vid
+      remDr$sendKeysToActiveElement(list(key = "shift", "N"))
+      i = i + 1
+      Sys.sleep(sample(seq(7, 15, 1), 1))
+    }
+    
+    write.csv(all.titles, paste0("C:\\Users\\XPS\\Desktop\\",
+                                 gsub(pattern = ":", replacement = "_",
+                                      gsub(pattern = " ", replacement = "_", Sys.time())), 
+                                 "_",
+                                 number.of.vids,
+                                 "vids",
+                                 ".csv")
+    )
+    
+    remDr$close()
+  
+}
 
-remDr$close()
+search.vids.x.times <- function(search, number.of.vids, number.of.iterations) {
+  # iterates through the search.vids function n number of times, for gathering empirical data
+  i <- 0
+  while (i < number.of.iterations) {
+    search.vids(search, number.of.vids)
+    i <- i + 1
+    
+  }
+  
+  paste("All done! Ran ", number.of.iterations, " iterations")
+}
 
+fresh.vids.x.times <- function(number.of.vids, number.of.iterations) {
+  # iterates through the start.fresh function n number of times, for gathering empirical data
+  i <- 0
+  while (i < number.of.iterations) {
+    start.fresh(number.of.vids)
+    i <- i + 1
+  }
+  
+  paste("All done! Ran ", number.of.iterations, " iterations")
+}
